0d43c429361d7645213bf9d7fab18775d1ece2e1
ITOpsSupport <88940779+ITOpsSupport@users.noreply.github.com>
2021-09-03 15:00:14 +0530

    Create ba2.sh
    to run this script type the command 
    $ sh ba2.sh

---
+++ .github/workflows/ba2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+mkdir BESClient
+cd BESClient
+wget https://raw.githubusercontent.com/BigFixadmin/BigFixAgent/main/BESAgent-10.0.1.41-ubuntu10.amd64.deb
+wget https://raw.githubusercontent.com/BigFixadmin/BigFixAgent/main/actionsite.afxm
+  dpkg -i BESAgent-10.0.1.41-ubuntu10.amd64.deb
+
+/etc/init.d/besclient start
+
+echo "Enable systcl parameters to disable it."
+echo "net.ipv6.conf.all.disable_ipv6=1" >>  /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6=1" >>  /etc/sysctl.conf
+echo "net.ipv6.conf.lo.disable_ipv6=1" >>  /etc/sysctl.conf
+echo "Making these changes effective to kernel"
+sysctl -p
+echo "creating rc.local with right command set"
+cat > /etc/rc.local <<EOF
+#!/bin/bash
+# /etc/rc.local
+
+/etc/sysctl.d
+/etc/init.d/procps restart
+
+exit 0
+EOF
+echo "Changing permission of the file to 755"
+chmod 755 /etc/rc.local     
+
+
+#sh -x ba2.sh   
+#end
