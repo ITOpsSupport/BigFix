5f174a53fcfb5654a18b7ac2dc6eef9a7676d4e4
ITOpsSupport <88940779+ITOpsSupport@users.noreply.github.com>
2021-09-07 19:02:44 +0530

    Update ip.sh

--- .github/workflows/ip.sh
+++ .github/workflows/ip.sh
@@ -6,13 +6,6 @@ echo "net.ipv6.conf.default.disable_ipv6=1" >>  /etc/sysctl.conf
 echo "net.ipv6.conf.lo.disable_ipv6=1" >>  /etc/sysctl.conf
 echo "Making these changes effective to kernel"
 sysctl -p
-echo "creating rc.local with right command set"
-cat > /etc/rc.local <<EOF
-# /etc/rc.local
-/etc/sysctl.d
-/etc/init.d/procps restart
-echo "Changing permission of the file to 755"
-chmod 755 /etc/rc.local
 echo "AddressFamily inet" >>  /etc/ssh/ssh_config
 /etc/init.d/besclient restart
 exit 0
